e2556c51583d7a6081bac069b8b0d78bebc425f0
Kishore <kishore_kumar-tw@external.mckinsey.com>
2016-01-20 16:13:19 +0530

    New spike for celery

---
+++ spikes/SimulationRunner/Runner.py
@@ -0,0 +1,15 @@
+import random
+import time
+from celery import Celery
+app = Celery('tasks', backend='rpc://', broker='amqp://rabbituser:password@mynix.cloudapp.net//')
+
+
+@app.task
+def run(x):
+    random_gen = random.Random()
+    print "Stating simulation run"
+    random_sleep_time = random_gen.randint(100, 1000)
+    print "Sleeping for ", random_sleep_time, " seconds"
+    time.sleep(random_sleep_time)
+    print "Done sleeping"
+    return x
